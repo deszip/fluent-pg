b0cc5d2a210406c10cfb0eaa0364e0cc75e59896
Deszip <raf.rafiki@gmail.com>
2016-03-13 17:28:53 +0200

    - Package.swift fixed

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "FluentPGDriver",
     dependencies: [
-        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0)
+        .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0),
         .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
     ]
 )
