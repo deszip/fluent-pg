6234c720be48659a9caf05523c5243d1db84356c
Deszip <raf.rafiki@gmail.com>
2016-03-20 12:12:54 +0200

    added fluent to test dependencies section

--- Package.swift
+++ Package.swift
@@ -4,6 +4,8 @@ let package = Package(
     name: "FluentPGDriver",
     dependencies: [
         .Package(url: "https://github.com/stepanhruda/PostgreSQL-Swift.git", majorVersion: 0),
+    ],
+    testDependencies: [
         .Package(url: "https://github.com/tannernelson/fluent.git", majorVersion: 0)
     ]
 )
